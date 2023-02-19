f09e348170b508515dfb5ff800e35fd8f3cc421a
wtrombly <will@therealestateexaminer.com>
2023-02-19 10:44:01 -0600

    removed template files
    removed:
     order.ts
    orderRouter.ts
    customer.ts and js
    order.ts and js
    product.ts
    edited app.ts to remove entrypoint for orders

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,14 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import * as bodyParser from "body-parser";
-import {orderRouter} from "./routes/orderRouter";
+
 import { appraiserRouter } from "./routes/appraiserRouter";
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
-app.use("/orders", orderRouter);
+
 app.use("/appraisers", appraiserRouter);
 
 
--- server/types/customer.ts
+++
@@ -1,9 +0,0 @@
-export interface BasicCustomer {
-  id: number,
-}
-
-export interface Customer extends BasicCustomer{
-  name: string,
-  email?: string,
-  password?: string
-}
