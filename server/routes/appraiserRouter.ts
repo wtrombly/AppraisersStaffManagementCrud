f9aae29e5476418c7dc39d0b12ee4ae2a644b4a8
wtrombly <will@therealestateexaminer.com>
2023-02-19 08:31:56 -0600

    Create appraiserRouter.ts

---
+++ server/routes/appraiserRouter.ts
@@ -0,0 +1,49 @@
+
+import express, {Request, Response} from "express";
+import * as appraiserModel from "../models/appraiser";
+import {Appraiser, BasicAppraiser} from "../types/appraiser";
+const orderRouter = express.Router();
+
+orderRouter.get("/", async (req: Request, res: Response) => {
+  appraiserModel.findAll((err: Error, appraisers: Appraiser[]) => {
+    if (err) {
+      return res.status(500).json({"errorMessage": err.message});
+    }
+
+    res.status(200).json({"data": appraisers});
+  });
+});
+
+orderRouter.post("/", async (req: Request, res: Response) => {
+  const newAppraiser: Appraiser = req.body;
+  appraiserModel.create(newAppraiser, (err: Error, emp_id: number) => {
+    if (err) {
+      return res.status(500).json({"message": err.message});
+    }
+
+    res.status(200).json({"emp_id": emp_id});
+  });
+});
+
+orderRouter.get("/:id", async (req: Request, res: Response) => {
+  const emp_id: number = Number(req.params.id);
+  appraiserModel.findOne(emp_id, (err: Error, appraiser: Appraiser) => {
+    if (err) {
+      return res.status(500).json({"message": err.message});
+    }
+    res.status(200).json({"data": appraiser});
+  })
+});
+
+orderRouter.put("/:id", async (req: Request, res: Response) => {
+  const appraiser: Appraiser = req.body;
+  appraiserModel.update(appraiser, (err: Error) => {
+    if (err) {
+      return res.status(500).json({"message": err.message});
+    }
+
+    res.status(200).send();
+  })
+});
+
+export {orderRouter};
