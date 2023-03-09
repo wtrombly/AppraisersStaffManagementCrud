fc7d56afeb19dfa995e05c46ac93efcc0d456925
wtrombly <will@therealestateexaminer.com>
2023-03-08 20:48:10 -0600

    moving buttons
    removed add and assign order from the main app
    created Appraiser Table and orders Table buttons
    
    added some css and override higher selector

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppraisersViewComponent } from './appraisers-view/appraisers-view.component';
 import { OrdersViewComponent } from './orders-view/orders-view.component';
 
 
 const routes: Routes = [
-  { path: 'orders', component: OrdersViewComponent }
+  { path: 'orders', component: OrdersViewComponent },
+  { path: 'appraisers', component: AppraisersViewComponent }
 ];
 
 @NgModule({
