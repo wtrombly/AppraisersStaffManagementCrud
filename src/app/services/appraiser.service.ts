d32aa317363268071846eda110fe01da957b279b
wtrombly <will@therealestateexaminer.com>
2023-03-12 10:35:27 -0500

    Remove paginator and MatSort from appraiser service
    these items were unused

--- src/app/services/appraiser.service.ts
+++ src/app/services/appraiser.service.ts
@@ -2,17 +2,14 @@ import { Injectable, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Appraiser } from '../models/appraiser';
 import { States } from '../models/states';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+
 import { ApiService } from './api.service';
-import { Subject, Observable } from 'rxjs';
+import {  Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppraiserService implements OnInit{
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
 
   states = States;
   dataSource!: MatTableDataSource<Appraiser>;
